9b7976a3e884184f380403bd42a83136e330d5c0
khanhvecto <89617684+khanhvecto@users.noreply.github.com>
2024-01-02 23:28:12 +0700

    fix

--- FeatureFunctions/UpdateCopyStatus.sql
+++ FeatureFunctions/UpdateCopyStatus.sql
@@ -2,14 +2,15 @@
 -- Feature update copy status
 -- 
 CREATE OR REPLACE FUNCTION feature_update_copy_status(_copy_id int, _status int, _copy_position varchar)
-RETURNS TABLE (copy_id int, status int, copy_position varchar)
+RETURNS SETOF material_copies
 AS
 $$
 	BEGIN
-		UPDATE material_copies
-		SET status = _status, copy_position = _copy_position
-		WHERE copy_id = _copy_id
-		RETURNING copy_id, status, copy_position;
+		RETURN QUERY
+			UPDATE material_copies
+			SET status = _status, copy_position = _copy_position
+			WHERE copy_id = _copy_id
+			RETURNING copy_id, material_id, status, copy_position;
 	END;
 $$
 LANGUAGE plpgsql;

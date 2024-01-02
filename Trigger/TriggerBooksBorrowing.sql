46253238b10327304fac570fc530a0951d2cea7c
khanhvecto <89617684+khanhvecto@users.noreply.github.com>
2024-01-02 21:54:49 +0700

    add trigger books_borrowing

---
+++ Trigger/TriggerBooksBorrowing.sql
@@ -0,0 +1,19 @@
+-- 
+-- Trigger change copy status
+-- 
+CREATE OR REPLACE FUNCTION update_material_copies_status() 
+RETURNS TRIGGER 
+AS 
+$$
+	BEGIN
+		UPDATE material_copies
+		SET status = '2'
+		WHERE copy_id = NEW.copy_id;
+		RETURN NEW;
+	END;
+$$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_add_book_borrow_record
+AFTER INSERT ON books_borrowing
+FOR EACH ROW EXECUTE PROCEDURE update_material_copies_status();
